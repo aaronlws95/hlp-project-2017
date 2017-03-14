2a612633645943d7d720dae24743364bb4bac7cb
Santiago Rubio <spr13@imperial.ac.uk>
2017-03-14 12:43:58 +0000

    new cast functions for S and cond

--- hlp_project_2017/Cast.fs
+++ hlp_project_2017/Cast.fs
@@ -162,36 +162,81 @@ module Cast=
         | "" -> None |> Some
         | _ -> None
     //No forget DCD, EQU, FILL and END
-    (*
-    let (|IsInstruction|_|) =
-        function
-        | "MOV" -> "MOV" |> Some
-        | "B" -> "B" |> Some
-        | _ -> None
 
-    let (|IsCond|_|) =
-        function
-        | "EQ" -> "EQ" |> Some
-        | "LS" -> "LS" |> Some
-        | _ -> None
+    let (|Prefix|_|) (p:string) (s:string) =
+        if s.StartsWith(p) then
+            Some(s.Substring(p.Length))
+        else
+            None
 
-    *)
+    let checkS (s:string) =
+        if s <> ""
+        then 
+            if s.[0] ='S'
+            then "S"
+            else ""
+        else ""
+    let checkCond (s:string) =
+        if s.Length = 2
+        then s
+        elif s.Length = 3
+        then s.[1..2]
+        elif s = "S"
+        then ""
+        else s
+
+    let toTuple (inst:string) (rest:string) = [inst; checkS rest; checkCond rest]
+
+    let TokenizeInst (s:string)=
+        match s with
+        | Prefix "MOV" rest -> toTuple "MOV" rest
+        | Prefix "MVN" rest -> toTuple "MVN" rest
+        | Prefix "ADD" rest -> toTuple "ADD" rest
+        | Prefix "ADC" rest -> toTuple "ADC" rest
+        | Prefix "SUB" rest -> toTuple "SUB" rest
+        | Prefix "SBC" rest -> toTuple "SBC" rest
+        | Prefix "RSB" rest -> toTuple "RSB" rest
+        | Prefix "RSC" rest -> toTuple "RSC" rest
+        | Prefix "AND" rest -> toTuple "AND" rest
+        | Prefix "EOR" rest -> toTuple "EOR" rest
+        | Prefix "BIC" rest -> toTuple "BIC" rest
+        | Prefix "ORR" rest -> toTuple "ORR" rest
+        | Prefix "LSL" rest -> toTuple "LSL" rest
+        | Prefix "LSR" rest -> toTuple "LSR" rest
+        | Prefix "ASR" rest -> toTuple "ASR" rest
+        | Prefix "ROR" rest -> toTuple "ROR" rest
+        | Prefix "CMP" rest -> toTuple "CMP" rest
+        | Prefix "CMN" rest -> toTuple "CMN" rest
+        | Prefix "TST" rest -> toTuple "TST" rest
+        | Prefix "TEQ" rest -> toTuple "TEQ" rest
+        | Prefix "B" rest -> toTuple "B" rest
+        | Prefix "B" rest -> toTuple "BL" rest
+        | x -> [x; ""; ""]
+      
+
+    
+
     let ParseInstruction (strlist:string list)=
-        let instruction = strlist.[0]
-        let basicinstruction = instruction.[0..2]
-        let setflag = 
-            if instruction.Length = 4 then (string)instruction.[3]
+        let instruction = TokenizeInst strlist.[0]
+        let basicinstruction = instruction.[0]//instruction.[0..2]
+        let setflag = instruction.[1]
+            (*if instruction.Length = 4 then (string)instruction.[3]
             elif instruction.Length = 6 then (string)instruction.[3]
-            else ""
-        let condition = 
-            if instruction.Length = 5 then instruction.[3..4]
+            else ""*)
+        let condition = instruction.[2]
+            (*if instruction.Length = 5 then instruction.[3..4]
             elif instruction.Length = 6 then instruction.[4..5]
-            else ""
+            else ""*)
         let instrline = basicinstruction::setflag::condition::(strlist.Tail)
         match instrline with
+        //normal with S or Cond or neither
         | [ IsMOVInst inst; IsSetFlag sf; IsCondition cond; IsReg dest; IsRegOrLit op1 ] -> Line(ALU(inst(dest,op1),sf),None,cond)
         | [ IsALUInst inst; IsSetFlag sf; IsCondition cond; IsReg dest; IsReg op1; IsRegOrLit op2 ] -> Line(ALU(inst(dest,op1,op2),sf),None,cond)
         | [ IsShiftInst inst;  IsSetFlag sf; IsCondition cond;IsReg dest; IsReg op1; IsRegOrLit op2] -> Line(SHIFT(inst(dest,op1,op2),sf),None,cond)
         | [ IsCOMPInst inst; IsSetFlag sf; IsCondition cond;IsReg dest; IsRegOrLit op1] -> Line(SF(inst(dest,op1)),None,cond)
-        //| [ IsBranchInst inst; IsCondition cond; IsLabel lab] -> 
+        //| [ IsBranchInst inst; IsCondition cond; IsLabel lab] ->
+        // with shifts
+        | [ IsMOVInst inst; IsSetFlag sf; IsCondition cond; IsReg dest; IsReg op1; "," ; IsShiftInst shiftinst; IsRegOrLit exp] -> Line(ALU(inst(dest,Reg(op1)),sf),shiftinst(op1,op1,exp)|> Some,cond)
+        | [ IsALUInst inst; IsSetFlag sf; IsCondition cond; IsReg dest; IsReg op1; IsReg op2 ; "," ; IsShiftInst shiftinst; IsRegOrLit exp] -> Line(ALU(inst(dest,op1,Reg(op2)),sf),shiftinst(op2,op2,exp)|> Some,cond)
+        | [ IsCOMPInst inst; IsSetFlag sf; IsCondition cond;IsReg dest; IsReg op1 ; "," ; IsShiftInst shiftinst; IsRegOrLit exp] -> Line(SF(inst(dest,Reg(op1))),shiftinst(op1,op1,exp)|> Some,cond)
         | x -> failwithf "Unexpected match in parser: %A" x
