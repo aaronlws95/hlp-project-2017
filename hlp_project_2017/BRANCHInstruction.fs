df92bf505e264e9eb8e9ba7ce091d6c1a6483786
JunShern <chanjunshern@gmail.com>
2017-03-19 16:09:41 +0000

    Small readability improvement in BRANCHinst

--- hlp_project_2017/BRANCHInstruction.fs
+++ hlp_project_2017/BRANCHInstruction.fs
@@ -13,6 +13,7 @@ module BRANCHInstruction =
             match instruction with
             | B(Addr(target)) -> Map.add (R 15) (target) state.RegMap // Replace R15 (PC) with target address
             | BL(Addr(target)) -> 
-                Map.add (R 14) (state.RegMap.[R 15] + 4) state.RegMap // Replace R14 (LR) with PC+4
-                |> Map.add (R 15) (target) // Replace R15 (PC) with target address, 
+                state.RegMap 
+                |> Map.add (R 14) (state.RegMap.[R 15] + 4) // Replace R14 (LR) with PC+4
+                |> Map.add (R 15) (target) // Replace R15 (PC) with target address
         {state with RegMap = newRegMap}
