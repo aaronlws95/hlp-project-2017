fb5474b773f04a4b057ed6b8c75a8031b945aab4
Tianyou Wang <wang.tianyou@gmail.com>
2017-03-19 03:30:45 +0000

    added package option to the gulp script

--- hlp_project_2017/UserInterface/gulpfile.js
+++ hlp_project_2017/UserInterface/gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require("gulp");
 const clean = require("gulp-clean");
 const shell = require("gulp-shell");
 const path = require("path");
-const workflow = ["clean", "build", "test"];
+const workflow = ["clean", "build"];
 
 gulp.task("default", () => {
     gulp.start("run");
@@ -10,12 +10,16 @@ gulp.task("default", () => {
 
 gulp.task("run", () => {
     gulp.start(workflow);
+    gulp.watch("../**/*.fs").on("change", (event) => {
+        console.log(`File <${path.basename(event.path)}> was ${event.type}`);
+    });
 })
 
 gulp.task("clean", () => {
     return gulp.src([
         "./*.js",
-        "!./gulpfile.js"
+        "!./gulpfile.js",
+        "!./main.js"
     ]).pipe(clean())
 })
 
@@ -27,6 +31,7 @@ gulp.task("test", ["build"], () => {
 
 });
 
-gulp.watch("../**/*.fs", ["run"]).on("change", (event) => {
-    console.log(`File <${path.basename(event.path)}> was ${event.type}`);
-});
+gulp.task("package", ["build"], shell.task([
+    "electron-packager . --appname=ARMEmulator --asar=true --out=electron --overwrite"
+]));
+
