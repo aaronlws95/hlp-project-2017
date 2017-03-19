88d2b78ba9831dc78a46d16aba1a3ef06f0fc0e3
JunShern <chanjunshern@gmail.com>
2017-03-19 15:54:59 +0000

    Add BRANCHInstruction.fs, integrate into executeLine

---
+++ hlp_project_2017/BRANCHInstruction.fs
@@ -0,0 +1,18 @@
+﻿namespace ARM7TDMI
+
+/// ===========================================
+/// Branching functions
+/// ===========================================
+
+module BRANCHInstruction = 
+    open InstructionType 
+    open MachineState
+    /// execute set flag instruction
+    let executeInstruction state instruction = 
+        let newRegMap = 
+            match instruction with
+            | B(Addr(target)) -> Map.add (R 15) (target) state.RegMap // Replace R15 (PC) with target address
+            | BL(Addr(target)) -> 
+                Map.add (R 14) (state.RegMap.[R 15] + 4) state.RegMap // Replace R14 (LR) with PC+4
+                |> Map.add (R 15) (target) // Replace R15 (PC) with target address, 
+        {state with RegMap = newRegMap}
