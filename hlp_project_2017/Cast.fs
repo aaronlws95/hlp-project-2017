b3753117df1fd3fac6a01ffc6a022301e8e1c1de
Santiago Rubio <spr13@imperial.ac.uk>
2017-03-20 19:17:03 +0000

    Added LABELInstructions in parser

--- hlp_project_2017/Cast.fs
+++ hlp_project_2017/Cast.fs
@@ -4,11 +4,20 @@ module Cast=
     open InstructionType
     open MachineState
 
+
     /// matches string (X -> X) returning integer
     let (|IsInt|_|) (s:string) = 
         try 
             int s |> Some
         with _ -> None
+
+    /// matches string list returning Int list
+    let rec (|IsIntList|_|) (lst:string list) = 
+        match lst with
+        | IsInt value::IsIntList rest -> value::rest |> Some
+        | [IsInt value] -> [value] |> Some
+        | [] -> [] |> Some
+        | _ -> None
 
     ///matches string (#X->X) returning integer
     let (|IsLit|_|) (s:string) = 
@@ -61,14 +70,10 @@ module Cast=
 
     ///matches Addr label or addr literal
     let (|IsAddr|_|) (b_map:Map<string,Address>) (s:string) = 
-        let getX = s.[1..] 
-        if s.[0]='#'
-        then
-            match getX with
-            | IsInt x -> Some(Addr(x))
-            | _ -> None
-        else
-            b_map.TryFind(s)
+        match s with
+        | IsInt x -> Some(Addr(x))
+        | x -> b_map.TryFind(x)
+            
 
     //cast value to addr
     let ValueToAddr (value:Value) =
@@ -113,7 +118,7 @@ module Cast=
         | "SBC" -> SBC |> Some
         | "RSB" -> RSB |> Some
         | "RSC" -> RSC |> Some
-        //| "AND" -> AND |> Some
+        | "AND" -> AND |> Some
         | "EOR" -> EOR |> Some
         | "BIC" -> BIC |> Some
         | "ORR" -> ORR |> Some
@@ -219,56 +224,66 @@ module Cast=
             None
 
     let checkS_or_B (s:string) =
-        if s <> ""
-        then 
-            if s.[0] ='S'
-            then "S"
-            elif s.[0] ='B'
-            then "B"
-            else ""
-        else ""
-    let checkCond (s:string) =
-        if s.Length = 2
-        then s
-        elif s.Length = 3
-        then s.[1..2]
-        elif s = "S"
-        then ""
-        elif s = "B"
-        then ""
-        else s
+        match (if s.Length>0 then s.[0] else ' ') with
+        | 'S' when s.Length>1 -> ["S";s.[1..]]
+        | 'B' when s.Length>1 -> ["B";s.[1..]]
+        | _   when s.Length>1 -> ["";s.[1..]]
+        | 'S' -> ["S";""]
+        | 'B' -> ["B";""]
+        | _   -> ["";""]
+    let checkdir (s:string) =
+        match (if s.Length>1 then s.[0..1] else "") with
+        | "ED" when s.Length>2 -> ["ED" ;s.[2..]]
+        | "IB" when s.Length>2 -> ["IB" ;s.[2..]]
+        | "FD" when s.Length>2 -> ["FD" ;s.[2..]]
+        | "IA" when s.Length>2 -> ["IA" ;s.[2..]]
+        | "EA" when s.Length>2 -> ["EA" ;s.[2..]]
+        | "DB" when s.Length>2 -> ["DB" ;s.[2..]]
+        | "FA" when s.Length>2 -> ["FA" ;s.[2..]]
+        | "DA" when s.Length>2 -> ["DA" ;s.[2..]]
+        | "ED"  -> ["ED" ;""]
+        | "IB"  -> ["IB" ;""]
+        | "FD"  -> ["FD" ;""]
+        | "IA"  -> ["IA" ;""]
+        | "EA"  -> ["EA" ;""]
+        | "DB"  -> ["DB" ;""]
+        | "FA"  -> ["FA" ;""]
+        | "DA"  -> ["DA" ;""]
+        | _ -> [""; s]
 
-    let toTuple (inst:string) (rest:string) = [inst; checkS_or_B rest; checkCond rest]
+    let inst_S_or_B_Cond_ToList (inst:string) (rest:string) = inst::checkS_or_B rest
+    let inst_dir_Cond_ToList (inst:string) (rest:string) = inst::checkdir rest
 
     let TokenizeInst (s:string)=
         match s with
-        | Prefix "MOV" rest -> toTuple "MOV" rest   //1
-        | Prefix "MVN" rest -> toTuple "MVN" rest   //2
-        | Prefix "ADD" rest -> toTuple "ADD" rest   //3
-        | Prefix "ADC" rest -> toTuple "ADC" rest   //4
-        | Prefix "SUB" rest -> toTuple "SUB" rest   //5
-        | Prefix "SBC" rest -> toTuple "SBC" rest   //6
-        | Prefix "RSB" rest -> toTuple "RSB" rest   //7
-        | Prefix "RSC" rest -> toTuple "RSC" rest   //8
-        | Prefix "AND" rest -> toTuple "AND" rest   //9
-        | Prefix "EOR" rest -> toTuple "EOR" rest   //10
-        | Prefix "BIC" rest -> toTuple "BIC" rest   //11
-        | Prefix "ORR" rest -> toTuple "ORR" rest   //12
-        | Prefix "LSL" rest -> toTuple "LSL" rest   //13
-        | Prefix "LSR" rest -> toTuple "LSR" rest   //14
-        | Prefix "ASR" rest -> toTuple "ASR" rest   //15
-        | Prefix "ROR" rest -> toTuple "ROR" rest   //16
-        | Prefix "RRX" rest -> toTuple "ROR" rest   //17
-        | Prefix "CMP" rest -> toTuple "CMP" rest   //18
-        | Prefix "CMN" rest -> toTuple "CMN" rest   //19
-        | Prefix "TST" rest -> toTuple "TST" rest   //20
-        | Prefix "TEQ" rest -> toTuple "TEQ" rest   //21
-        | Prefix "BL" rest -> toTuple "BL" rest     //22
-        | Prefix "B" rest -> toTuple "B" rest       //23
-        | Prefix "ADR" rest -> toTuple "ADR" rest   //24
-        | Prefix "LDR" rest -> toTuple "LDR" rest   //25 - double
-        | Prefix "STR" rest -> toTuple "STR" rest   //26
-        | Prefix "LDM" rest -> toTuple "LDM" rest   //27
-        | Prefix "STM" rest -> toTuple "STM" rest   //28
+        | Prefix "MOV" rest -> inst_S_or_B_Cond_ToList "MOV" rest   //1
+        | Prefix "MVN" rest -> inst_S_or_B_Cond_ToList "MVN" rest   //2
+        | Prefix "ADD" rest -> inst_S_or_B_Cond_ToList "ADD" rest   //3
+        | Prefix "ADC" rest -> inst_S_or_B_Cond_ToList "ADC" rest   //4
+        | Prefix "SUB" rest -> inst_S_or_B_Cond_ToList "SUB" rest   //5
+        | Prefix "SBC" rest -> inst_S_or_B_Cond_ToList "SBC" rest   //6
+        | Prefix "RSB" rest -> inst_S_or_B_Cond_ToList "RSB" rest   //7
+        | Prefix "RSC" rest -> inst_S_or_B_Cond_ToList "RSC" rest   //8
+        | Prefix "AND" rest -> inst_S_or_B_Cond_ToList "AND" rest   //9
+        | Prefix "EOR" rest -> inst_S_or_B_Cond_ToList "EOR" rest   //10
+        | Prefix "BIC" rest -> inst_S_or_B_Cond_ToList "BIC" rest   //11
+        | Prefix "ORR" rest -> inst_S_or_B_Cond_ToList "ORR" rest   //12
+        | Prefix "LSL" rest -> inst_S_or_B_Cond_ToList "LSL" rest   //13
+        | Prefix "LSR" rest -> inst_S_or_B_Cond_ToList "LSR" rest   //14
+        | Prefix "ASR" rest -> inst_S_or_B_Cond_ToList "ASR" rest   //15
+        | Prefix "ROR" rest -> inst_S_or_B_Cond_ToList "ROR" rest   //16
+        | Prefix "RRX" rest -> inst_S_or_B_Cond_ToList "ROR" rest   //17
+        | Prefix "CMP" rest -> inst_S_or_B_Cond_ToList "CMP" rest   //18
+        | Prefix "CMN" rest -> inst_S_or_B_Cond_ToList "CMN" rest   //19
+        | Prefix "TST" rest -> inst_S_or_B_Cond_ToList "TST" rest   //20
+        | Prefix "TEQ" rest -> inst_S_or_B_Cond_ToList "TEQ" rest   //21
+        | Prefix "BL" rest -> inst_S_or_B_Cond_ToList "BL" rest     //22
+        | Prefix "B" rest -> inst_S_or_B_Cond_ToList "B" rest       //23
+        | Prefix "ADR" rest -> inst_S_or_B_Cond_ToList "ADR" rest   //24
+        | Prefix "LDR" rest -> inst_S_or_B_Cond_ToList "LDR" rest   //25 - double
+        | Prefix "STR" rest -> inst_S_or_B_Cond_ToList "STR" rest   //26
+        | Prefix "LDM" rest -> inst_dir_Cond_ToList "LDM" rest   //27
+        | Prefix "STM" rest -> inst_dir_Cond_ToList "STM" rest   //28
+        | Prefix "END" rest -> inst_S_or_B_Cond_ToList "END" rest   
         | x -> [x; ""; ""]
    
--- hlp_project_2017/Parser.fs
+++ hlp_project_2017/Parser.fs
@@ -4,6 +4,12 @@ module Parser=
     open InstructionType
     open MachineState
     open Cast
+
+    type Map_addr_counter = 
+        {
+        Counter: int    //if user tries to use any memory below this address throw error
+        AddrMap : Map<string, Address>   //PC is R15
+        }
     
     let whiteSpace = [| ' '; '\f'; '\t'; '\r'; '\n'; ',' |] 
 
@@ -26,28 +32,34 @@ module Parser=
         let branch_map =
             let chooseAddr (b:Map<string,Address>) i =
                 match lineList.[i] with
-                | IsLabel x::IsLabel _::IsReg _::_ -> b.Add(x,(Addr (i*4)))
+                | IsLabel x::IsLabel endInst::_ when endInst.Contains("END") -> b.Add(x,(Addr (i*4)))
+                | IsLabel x::IsLabel _::IsReg _::_ -> b.Add(x,(Addr (i*4))) //ALL instructions except B BL DCD EQU FILL END
                 | _ -> b   
             seq { 0 .. lineList.Length - 1 } 
             |> Seq.fold chooseAddr Map.empty
 
         let remove_branch_label line=
             match line with
+            | IsLabel x::IsLabel endInst::_ when endInst.Contains("END") -> line.Tail
             | IsLabel _::IsLabel _::IsReg _::_ -> line.Tail
             | x -> x   
 
+        let label_map =
+            let chooseAddr (l:Map_addr_counter) i =
+                match lineList.[i] with
+                | IsLabel x::"FILL"::IsInt value::_ -> { l with AddrMap=l.AddrMap.Add(x,Addr(l.Counter)) ; Counter=l.Counter+value/4+1 }
+                | "FILL"::IsInt value::_ -> { l with AddrMap=l.AddrMap.Add("DEFAULT_FILL",Addr(l.Counter)) ; Counter=l.Counter+value/4+1 }
+                | IsLabel x::"DCD"::rest ->  { l with AddrMap=l.AddrMap.Add(x,Addr(l.Counter)) ; Counter=l.Counter+rest.Length+1 }
+                | IsLabel x::"EQU"::_ -> { l with AddrMap=l.AddrMap.Add(x,Addr(l.Counter)) ; Counter=l.Counter+1 }
+                | _ -> l   
+            (seq { 0 .. lineList.Length - 1 } |> Seq.fold chooseAddr { Counter=lineList.Length; AddrMap=Map.empty}).AddrMap
+
         let executeWordsAsCommand (strlist:string list)= //: InstructionLine
             let instruction = TokenizeInst strlist.[0]
-            let basicinstruction = instruction.[0]//instruction.[0..2]
-            let setflag_or_byte = instruction.[1]
-                (*if instruction.Length = 4 then (string)instruction.[3]
-                elif instruction.Length = 6 then (string)instruction.[3]
-                else ""*)
+            let basicinstruction = instruction.[0]
+            let setflag_or_byte_or_dir = instruction.[1]
             let condition = instruction.[2]
-                (*if instruction.Length = 5 then instruction.[3..4]
-                elif instruction.Length = 6 then instruction.[4..5]
-                else ""*)
-            let instrline = basicinstruction::setflag_or_byte::condition::(strlist.Tail)
+            let instrline = basicinstruction::setflag_or_byte_or_dir::condition::(strlist.Tail)
             match instrline with
             //normal with S or Cond or neither
             | [ IsMOVInst inst; IsSetFlag sf; IsCondition cond; IsReg dest; IsRegOrLit op1 ] -> Line(ALU(inst(dest,op1),sf),None,CondCast cond)
@@ -61,18 +73,26 @@ module Parser=
             | [ IsALUInst inst; IsSetFlag sf; IsCondition cond; IsReg dest; IsReg op1; IsReg op2 ; IsShiftInst shiftinst; IsRegOrLit exp] -> Line(ALU(inst(dest,op1,Reg(op2)),sf),shiftinst(op2,op2,exp)|> Some,CondCast cond)
             | [ IsCOMPInst inst; IsSetFlag sf; IsCondition cond;IsReg dest; IsReg op1 ; IsShiftInst shiftinst; IsRegOrLit exp] -> Line(SF(inst(dest,Reg(op1))),shiftinst(op1,op1,exp)|> Some,CondCast cond)
             //Memory Instructions
-            | [ "ADR"; IsSetFlag sf; IsCondition cond; IsReg dest; IsAddr branch_map addr] -> Line(MEM(ADR(dest,addr)),None,CondCast cond)
-            //| [ "LDR"; IsSetFlag sf; IsCondition cond; IsReg dest; "="; IsAddr branch_map addr] -> Line(MEM(LDR(dest,addr)),None,CondCast cond)
+            | [ "ADR"; IsSetFlag sf; IsCondition cond; IsReg dest; IsAddr label_map addr] -> Line(MEM(ADR(dest,addr)),None,CondCast cond)
+            | [ "LDR"; IsSetFlag sf; IsCondition cond; IsReg dest; "="; IsAddr label_map addr] -> Line(MEM(ADR(dest,addr)),None,CondCast cond)
             //LDR STR
             | [ IsMEMRInst inst; IsByteMode b; IsCondition cond; IsReg dest; "[" ; IsReg source; "]" ] ->  Line(MEM(inst(dest,source,Lit 0,Lit 0,b)),None,CondCast cond)
             | [ IsMEMRInst inst; IsByteMode b; IsCondition cond; IsReg dest; "[" ; IsReg source; IsRegOrLit offset; "]" ] ->  Line(MEM(inst(dest,source,offset,Lit 0,b)),None,CondCast cond)
             | [ IsMEMRInst inst; IsByteMode b; IsCondition cond; IsReg dest; "[" ; IsReg source; IsRegOrLit offset; "]"; "!" ] ->  Line(MEM(inst(dest,source,offset,offset,b)),None,CondCast cond)
             | [ IsMEMRInst inst; IsByteMode b; IsCondition cond; IsReg dest; "[" ; IsReg source; "]";  IsRegOrLit offset ] ->  Line(MEM(inst(dest,source,Lit 0,offset,b)),None,CondCast cond)
             //LDM STM
-            | [ IsMEMMInst inst; _ ; _ ; "[" ; IsLDMdir dir; "]"; IsCondition cond; IsReg source ] ->  Line(MEM(inst(dir,source,[],false)),None,CondCast cond)
-            | [ IsMEMMInst inst; _ ; _ ; "[" ; IsLDMdir dir; "]"; IsReg source ] ->  Line(MEM(inst(dir,source,[],false)),None,None)
-            | IsMEMMInst inst::_::_::"["::IsLDMdir dir::"]"::IsCondition cond::IsReg source::IsRegList reglist ->  Line(MEM(inst(dir,source,reglist,false)),None,CondCast cond)
-            | IsMEMMInst inst::_::_::"["::IsLDMdir dir::"]"::IsReg source::IsRegList reglist ->  Line(MEM(inst(dir,source,reglist,false)),None,None)
+            | [ IsMEMMInst inst; IsLDMdir dir; IsCondition cond; IsReg source ] ->  Line(MEM(inst(dir,source,[],false)),None,CondCast cond)
+            | [ IsMEMMInst inst; IsLDMdir dir; IsCondition cond; IsReg source; "!" ] ->  Line(MEM(inst(dir,source,[],true)),None,CondCast cond)
+            | IsMEMMInst inst::IsLDMdir dir::IsCondition cond::IsReg source::IsRegList reglist ->  Line(MEM(inst(dir,source,reglist,false)),None,CondCast cond)
+            | IsMEMMInst inst::IsLDMdir dir::IsCondition cond::IsReg source::"!"::IsRegList reglist ->  Line(MEM(inst(dir,source,reglist,true)),None,CondCast cond)
+            //LABEL Instructions
+            | [IsAddr label_map addr; ""; ""; "FILL"; IsInt value] -> Line(LABEL(FILL(addr,value/4)),None,None)
+            | [ "FILL";"";""; IsInt value] when label_map.TryFind("DEFAULT_FILL")<>None-> Line(LABEL(FILL(label_map.["DEFAULT_FILL"],value/4)),None,None)
+            | IsAddr label_map addr::""::""::"DCD"::IsIntList valueLst -> Line(LABEL(DCD(addr,valueLst)),None,None)
+            | [IsAddr label_map addr;"";""; "EQU"; IsInt value] -> Line(LABEL(EQU(value)),None,None)
+            //END
+            | ["END"; IsCondition cond] -> Line(END,None,CondCast cond)
+            //Syntax Error
             | x -> Failed_Parsing("Unexpected match in parser: "+(x |> String.concat " "))
 
         let instList = //: InstructionType list = 
@@ -89,6 +109,7 @@ module Parser=
             |> Seq.fold chooseAddr Map.empty
 
         { 
+            //PC = Addr 0 // PC is now Reg 15
             END = Addr (4*instList.Length)
             RegMap = init_reg
             MemMap = init_memory
