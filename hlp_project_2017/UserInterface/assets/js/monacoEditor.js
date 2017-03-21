6f54405e2c8c43f83cdbe6d93cf22c577c8261eb
Tianyou Wang <wang.tianyou+github@gmail.com>
2017-03-21 14:25:50 +0000

    Added additional syntax highlight rules

--- hlp_project_2017/UserInterface/assets/js/monacoEditor.js
+++ hlp_project_2017/UserInterface/assets/js/monacoEditor.js
@@ -8,14 +8,15 @@ requirejs(['vs/editor/editor.main'], function () {
         tokenizer: {
             root: [
                 [/\b(r([0-9]|1[0-5])|(pc|sp|lr))\b/, "register"],
-                [/[#$=](-)?((0x[0-9a-f]+)|(0b[01]+)|(\d+))/, "number"],
-                [/\b(MOV|ADD|SUB|MVN|EOR|RSB|RSC|ADC|SBC|BIC|ORR|ADR)(S)?(EQ|NE|CS|HS|CC|LO|MI|PL|VS|VC|HI|LS|GE|LT|GT|LE|AL)?\b/, "arithmetic"],
+                [/[#$=]?(-)?((0x[0-9a-f]+)|(0b[01]+)|(\d+))/, "number"],
+                [/\b(MOV|ADD|SUB|MVN|EOR|RSB|RSC|ADC|SBC|BIC|ORR|AND|ADR)(S)?(EQ|NE|CS|HS|CC|LO|MI|PL|VS|VC|HI|LS|GE|LT|GT|LE|AL)?\b/, "arithmetic"],
                 //<Operation>{S}{<cond>} Rd, Operand2
                 [/\b(LSL|LSR|ASR|ROR|RRX)(S)?(EQ|NE|CS|HS|CC|LO|MI|PL|VS|VC|HI|LS|GE|LT|GT|LE|AL)?\b/, "shift"],
                 [/\b(CMP|CMN|TST|TEQ)(EQ|NE|CS|HS|CC|LO|MI|PL|VS|VC|HI|LS|GE|LT|GT|LE|AL)?\b/, "compare"],
                 [/\b(B|BL)(EQ|NE|CS|HS|CC|LO|MI|PL|VS|VC|HI|LS|GE|LT|GT|LE|AL)?\b/, "branch"],
                 [/\b(LDR|STR)(B)?(EQ|NE|CS|HS|CC|LO|MI|PL|VS|VC|HI|LS|GE|LT|GT|LE|AL)?\b/, "single-register"],
                 [/\b(LDM|STM)(ED|IB|FD|IA|EA|DB|FA|DA)?(EQ|NE|CS|HS|CC|LO|MI|PL|VS|VC|HI|LS|GE|LT|GT|LE|AL)?\b/, "multiple-register"],
+                [/\b(END|EQU|FILL|DCD)\b/,"others"]
             ],
         }
     });
@@ -34,7 +35,7 @@ requirejs(['vs/editor/editor.main'], function () {
             { token: 'branch', foreground: '536DFE', fontStyle: 'bold' },
             { token: 'single-register', foreground: '536DFE', fontStyle: 'bold' },
             { token: 'multiple-register', foreground: '536DFE', fontStyle: 'bold' },
-
+            { token: 'others', foreground: '536DFE', fontStyle: 'bold' },
             { token: 'comment', foreground: '808080' }
         ]
     });
